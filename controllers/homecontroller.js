4deb87a299ea613483f3b441b01d1842695697d5
Adarsh Singh <Adarshsingh933@gmail.com>
2023-06-20 20:32:06 +0530

    set up view engine

---
+++ controllers/homecontroller.js
@@ -0,0 +1,3 @@
+module.exports.home=function(req,res){
+   res.render('<h1>Home Page</h1>');
+};
--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app=express();
 const port=8000;
 
 app.use('/',require('./routes/index'));
+
+// set view engine 
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log(`server is not running error:${err}`);
